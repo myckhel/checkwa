8bedead7dd2f862a6fa9e2d9fb3e267e3c232d45
myckhel <myckhel1@hotmail.com>
2020-06-29 16:58:22 +0100

    created check class

--- src/Check.php
+++ src/Check.php
@@ -10,32 +10,37 @@ class Check
 {
   public $private_url = "https://w2.checkwa.com/check/";
   public $public_url  = "https://webservice.checkwa.com/";
+  public $hide_image  = 0;
 
-  function __construct($type = "public")
+  function __construct($type = "public", $config = [])
   {
-    $prefix = $type === 'private' ? "" : "private_";
+    $prefix = $type === 'private' ? "private_" : "";
 
     $this->type       = $type;
-    $this->url        = $type === 'private' $this->$private_url : $this->public_url;
+    $this->url        = $type === 'private' ? $this->private_url : $this->public_url;
     $this->apikey     = config("checkwa.{$prefix}apikey");
     $this->user       = config("checkwa.{$prefix}user");
-    $this->server     = config("checkwa.{$prefix}server");
-    $this->callback   = config("checkwa.{$prefix}callback");
+    $this->server     = config("checkwa.server");
+    $this->callback   = config("checkwa.callback");
   }
 
-  public function check($number, $code)
+  public function check($number, $code, $extra = [])
   {
     $response     = Http::withHeaders([
       'Content-Type' => 'application/json'
-    ])->post($this->url, [
+    ])->post($this->url, array_merge([
       'user'      => $this->user,
       'apikey'    => $this->apikey,
       'action'    => 'check',
-    ]);
+      'num'       => $number,
+      'cod'       => $code,
+      'callback'  => $this->callback,
+      'server'    => $this->server,
+      // 'token'     => $extra['token'] ?? null,
+      // 'hide_image'=> $extra['hide_image'] ?? 0,
+    ], $extra));
 
-    $res_json = $response->json();
-
-    return $res_json;
+    return $response->json();
   }
 }
 
